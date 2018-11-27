5c06267543dd71d78ef3bfa9bf478a38d158edd2
amitmore17 <amitmore17@gmail.com>
2018-11-27 13:55:32 +0530

    modified the code

--- read_data.py
+++ read_data.py
@@ -1,9 +1,20 @@
 import pickle
 path = './dataset/'
-exp_numbers = 18
-exp_data = []
-for exp in range(exp_numbers):
-	exp_file_name = path+'exp_'+str(exp)+'.pk'
-	file_object = open(exp_file_name,'rb') 
-	exp_data.append(pickle.load(file_object))
-	file_object.close()
+#total exp numbers = 18
+exp = 1
+exp_file_name = path+'exp_'+str(exp)+'.pk'
+file_object = open(exp_file_name,'rb') 
+exp_data = pickle.load(file_object)
+file_object.close()
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
